baf563c9b013078712d3b71bbcec541ffc9ea81e
minju7346 <minju7346@naver.com>
2022-01-10 23:21:25 +0900

    complete

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 def solution(lottos, win_nums):
+    num1 = lottos.count(0)
     lottos = set(lottos)
     win_nums = set(win_nums)
-    num = len(lottos&win_nums)
-    a = 6 - (num -1)
-    if a == 7:
-        a = 6
-    elif a == 6:
-        
-    b = a + 2 
-    
-    answer = [a, b]
+    num2 = len(lottos & win_nums)
+    min = 6 - (num2 - 1)
+    if min == 7:
+        min = 6
+    max = min - num1 + 1
+    if (max == 6) & (num1 < 2):
+        max = 6
+    answer = [min, max]
     return answer
-
